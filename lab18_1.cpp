d19732d5986b49fbfcedf64b06d11e1847a35c82
majespectre00 <oat601_12@hotmail.com>
2021-02-23 16:03:20 +0700

    lab18_1 completed

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,13 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect a, Rect b){
+	double xx1,xx2,yy1,yy2;
+	xx1 = max(a.x,b.x);
+	yy1 = min(b.y,a.y);
+	xx2 = min(a.x+a.w,b.x+b.w);
+	yy2 = max(b.y-b.h,a.y-a.h);
+	double over = (xx2-xx1)*(yy1-yy2);
+	if(over>0) return over;
+	else return 0;
 }
